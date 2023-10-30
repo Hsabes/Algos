217ea0de1f958ca48a6e0982f56749f8d8908e13
Harrison Sabean <92425655+Hsabes@users.noreply.github.com>
2023-10-30 17:59:23 -0400

    more algos honey

--- algos.js
+++ algos.js
@@ -220,3 +220,60 @@ function adjacentElementsProduct(array) {
 // ************************************************
 // ================================================
 
+// Bouncing Balls (6 Kyu)
+
+// A child is playing with a ball on the nth floor of a tall building. The height of this floor above ground level, h, is known.
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+// His mother looks out of a window 1.5 meters from the ground.
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing)?
+
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+// Note:
+// The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+// Examples:
+// - h = 3, bounce = 0.66, window = 1.5, result is 3
+
+// - h = 3, bounce = 1, window = 1.5, result is -1 
+
+// (Condition 2) not fulfilled).
+
+function bouncingBall(h,  bounce,  window) {
+  if (h <= 0 || (bounce <= 0 || bounce >= 1) || window >= h){
+    return -1;
+  }
+  let result = 0;
+  do {
+    result++
+    h = h * bounce;
+    if (h > window){
+      result++
+    } 
+  } while (h > window);
+  return result;
+}
+
+// https://www.codewars.com/kata/5544c7a5cb454edb3c000047 (g964)
+
+// ================================================
+// ************************************************
+// ================================================
+
+// Multiple of Index (8 Kyu) (Despite it being an easy problem, I really liked it!)
+
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+function multipleOfIndex(array) {
+  return array.filter((number, index) => number % index === 0 || number === 0);
+}
+
+// https://www.codewars.com/kata/5a34b80155519e1a00000009 (Dmitry Kudla)
+
+// ================================================
+// ************************************************
+// ================================================
