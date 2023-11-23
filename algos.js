22bf7d24dba2932e50102f61401ec1d1394321e9
Harrison Sabean <92425655+Hsabes@users.noreply.github.com>
2023-11-23 15:07:26 -0500

    revrot() and in progress: snakes and ladders

--- algos.js
+++ algos.js
@@ -751,3 +751,98 @@ multiplicationTable = function(size) {
 // ================================================================================================
 // ************************************************************************************************
 // ================================================================================================
+
+// Reverse or rotate? (6 Kyu)
+
+// The input is a string str of digits. 
+// Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+
+// If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. 
+// Put together these modified chunks and return the result as a string.
+
+// If:
+// sz is <= 0 or if str is empty return ""
+// sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+
+// Examples:
+// revrot("123456987654", 6) --> "234561876549"
+// revrot("123456987653", 6) --> "234561356789"
+// revrot("66443875", 4) --> "44668753"
+// revrot("66443875", 8) --> "64438756"
+// revrot("664438769", 8) --> "67834466"
+// revrot("123456779", 8) --> "23456771"
+// revrot("", 8) --> ""
+// revrot("123456779", 0) --> "" 
+// revrot("563000655734469485", 4) --> "0365065073456944"
+// Example of a string rotated to the left by one position:
+// s = "123456" gives "234561".
+
+function revrot(str, sz) {
+  if (sz <= 0 || !str || sz > str.length){
+    return '';
+  }
+  let chunks = [];
+  let strArr = str.split("")
+  for (let i = 0; i < str.length; i += sz){
+    let tempSum = 0;
+    let currSegment = strArr.slice(i, i + sz)
+    if (currSegment.length < sz){
+      continue;
+    }
+    currSegment.map((int) => tempSum += Math.pow(int, 3));
+    if (tempSum % 2 === 0){
+      chunks.push(currSegment.reverse().join(""));
+    } else {
+      chunks.push(currSegment.slice(1).join("") + currSegment[0]);
+    }
+  }
+  return chunks.join("");
+}
+
+// https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991 (g964)
+
+// ================================================================================================
+// ************************************************************************************************
+// ================================================================================================
+
+
+// for later:
+
+// player moves associated die rolls (die1 and die2)
+// if lands on ladder, move to associated location
+// if lands on snake, move to associated location
+
+// function SnakesLadders(die1, die2) {
+//   return die1 + die2;
+// };
+
+// SnakesLadders.prototype.play = function(die1, die2) {
+//   const ladders = {
+//     1: 36,
+//     6: 13,
+//     7: 30,
+//     14: 25,
+//     20: 41,
+//     27: 83,
+//     35: 43,
+//     50: 66,
+//     70: 90,
+//     77: 97,
+//     86: 93
+//   }
+//   const snakes = {
+//     15: 5,
+//     45: 24,
+//     48: 10,
+//     61: 18,
+//     63: 59,
+//     73: 52,
+//     88: 67,
+//     91: 87,
+//     94: 74,
+//     98: 79
+//   }
+//   return SnakesLadders(die1, die2)
+// }
+
+// https://www.codewars.com/kata/587136ba2eefcb92a9000027
