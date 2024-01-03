27180fe1b6453e76500875aad0f58637dab2cea9
Harrison Sabean <92425655+Hsabes@users.noreply.github.com>
2024-01-02 19:39:52 -0500

    updateInventory()

--- algos.js
+++ algos.js
@@ -1271,6 +1271,78 @@ function longestConsec(strarr, k) {
 // ************************************************************************************************
 // ================================================================================================
 
+// Clean up after your dog (7 Kyu)
+
+// You have stumbled across the divine pleasure that is owning a dog and a garden. Now time to pick up all the cr@p! :D
+
+// Given a 2D array to represent your garden, you must find and collect all of the dog cr@p - represented by '@'.
+
+// You will also be given the number of bags you have access to (bags), and the capactity of a bag (cap). If there are no bags then you can't pick anything up, so you can ignore cap.
+
+// You need to find out if you have enough capacity to collect all the cr@p and make your garden clean again.
+
+// If you do, return 'Clean', else return 'Cr@p'.
+
+// Watch out though - if your dog is out there ('D'), he gets very touchy about being watched. If he is there you need to return 'Dog!!'.
+
+// For example:
+
+// x=
+// [[_,_,_,_,_,_]
+// [_,_,_,_,@,_]
+// [@,_,_,_,_,_]]
+
+// bags = 2, cap = 2
+
+// return --> 'Clean'
+
+function crp(x, bags, cap){
+  let count = 0;
+  let flatArr = x.flat().filter((x) => x === '@' || x === 'D');
+  for (let i = 0; i < flatArr.length; i++){
+    if (flatArr[i] === 'D'){
+      return 'Dog!!';
+    }
+    count++;
+  }
+  return count > cap * bags ? 'Crp' : 'Clean';
+}
+
+// ================================================================================================
+// ************************************************************************************************
+// ================================================================================================
+
+function updateInventory(curStock, newStock) {
+  const stock = {};
+  const result = [];
+  
+  for (let i = 0; i < curStock.length; i++){
+    let item = curStock[i];
+    stock[item[1]] = item[0]
+  }
+  
+  for (let i = 0; i < newStock.length; i++){
+    let item = newStock[i];
+    let name = item[1];
+    let count = item[0];
+    if (stock[name]){
+      stock[name] += count;
+    } else {
+      stock[name] = count;
+    }
+  }
+  
+  for (const item in stock){
+    result.push([stock[item], item])
+  }
+  
+  return result.sort((x, y) => x[1].localeCompare(y[1]));
+}
+
+// ================================================================================================
+// ************************************************************************************************
+// ================================================================================================
+
 // for later:
 
 // player moves associated die rolls (die1 and die2)
